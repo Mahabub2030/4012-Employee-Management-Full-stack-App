e8d1cca03f95d24dfbce3cebd7206650d6ffa4c5
Mahaub Alam <mahabubalom72091@gmail.com>
2025-06-21 08:34:43 +0300

    added all code one time

--- client/src/components/Dashboard/Sidebar/Files.jsx
+++ client/src/components/Dashboard/Sidebar/Files.jsx
@@ -1,202 +1,204 @@
+
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 
-
 const pdfData = [
-  {
-    id: "407703",
-    name: "Abdul Hakim Omar Al-Kalde",
-    title: "Project Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "ID  For Abdul Hakim Omar Al-Kalde ",
-    status: "N/A",
-  },
-  {
-    id: "407703",
-    name: "Abdul Hakim Omar Al-Kalde",
-    title: "Project Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "Job Offer  For Abdul Hakim Omar Al-Kalde ",
-    status: "N/A",
-  },
-  {
-    id: "407703",
-    name: "Abdul Hakim Omar Al-Kalde",
-    title: "Project Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "Joining from For Abdul Hakim Omar Al-Kalde ",
-    status: "N/A",
-  },
-  {
-    id: "407703",
-    name: "Abdul Hakim Omar Al-Kalde",
-    title: "Project Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "Cv For Abdul Hakim Omar Al-Kalde ",
-    status: "N/A",
-  },
-
-  {
-    id: "401798",
-    name: "Shoeib Abou Zeid Mahmoud Awad",
-    title: "Operation Manager",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/10hpXcpAfxvaA38QVkoUR7hKeJRHTc_UB/view?usp=sharing",
-    description: "Passprot for Engr. Shoeib Abou Zeid Mahmoud Awad",
-    status: "A/V",
-  },
-  {
-    id: "401798",
-    name: "Shoeib Abou Zeid Mahmoud Awad",
-    title: "Operation Manager",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/1CjrJemtr1DNJZxL5A2ytuy5RitUaR5ob/view?usp=drive_link",
-    description: "Iqama for Engr. Shoeib Abou Zeid Mahmoud Awad",
-    status: "A/V",
-  },
-  {
-    id: "401798",
-    name: "Shoeib Abou Zeid Mahmoud Awad",
-    title: "Operation Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "Joining from for Engr. Shoeib Abou Zeid Mahmoud Awad",
-    status: "N/A",
-  },
-  {
-    id: "401798",
-    name: "Shoeib Abou Zeid Mahmoud Awad",
-    title: "Operation Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "Job for Engr. Shoeib Abou Zeid Mahmoud Awad",
-    status: "N/A",
-  },
-  {
-    id: "401798",
-    name: "Shoeib Abou Zeid Mahmoud Awad",
-    title: "Operation Manager",
-    group: "MANAGMENT",
-    file: "",
-    description: "CV for Engr. Shoeib Abou Zeid Mahmoud Awad",
-    status: "N/A",
-  },
-
-  {
-    id: "405721",
-    name: "Ashraf Ahmed Abdulhadi",
-    title: "Project Coordinator",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/1MT75u3FnDT4W4GDNz2LEbsb0sM1d8CZX/view?usp=drive_link",
-    description: "Iqama for Engr. Ashraf Ahmed Abdulhadi",
-    status: "A/V",
-  },
-  {
-    id: "405721",
-    name: "Ashraf Ahmed Abdulhadi",
-    title: "Project Coordinator",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
-    description: "Passprot for Engr. Ashraf Ahmed Abdulhadi",
-    status: "A/V",
-  },
-  {
-    id: "405721",
-    name: "Ashraf Ahmed Abdulhadi",
-    title: "Project Coordinator",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
-    description: "Job Offer for Engr. Ashraf Ahmed Abdulhadi",
-    status: "N/A",
-  },
-  {
-    id: "405721",
-    name: "Ashraf Ahmed Abdulhadi",
-    title: "Project Coordinator",
-    group: "MANAGMENT",
-    file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
-    description: "CV for Engr. Ashraf Ahmed Abdulhadi",
-    status: "N/A",
-  },
-  {
-    id: "403698",
-    name: "Ahmed Al-Dossary",
-    title: "Project Administrator",
-    group: "MANAGMENT",
-    file: "",
-    description: "CV for Ahmed Al-Dossary",
-    status: "N/A",
-  },
-  {
-    id: "403698",
-    name: "Ahmed Al-Dossary",
-    title: "Project Administrator",
-    group: "MANAGMENT",
-    file: "",
-    description: "JoB Offcer for Ahmed Al-Dossary",
-    status: "N/A",
-  },
-  {
-    id: "403698",
-    name: "Ahmed Al-Dossary",
-    title: "Project Administrator",
-    group: "MANAGMENT",
-    file: "",
-    description: "JoB Offcer for Ahmed Al-Dossary",
-    status: "N/A",
-  },
-
-  {
-    id: "407703",
-    name: "Amir Hossain",
-    title: "Foreman",
-    group: "Airport N & S",
-    file: "https://drive.google.com/file/d/1yvCeguxZt0vdrpRpQa1mtkx2DCrpqF8T/view?usp=drive_link",
-    description: "this one  only Iqama.",
-    status: "A/V",
-  },
-  {
-    id: "405406",
-    name: "ANTONIO JR ENARIO ",
-    title: "Supervisor Nursery",
-    group: "NURSERY",
-    file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
-    description: "Iqama for ANTONIO JR ENARIO.",
-    status: "A/V",
-  },
-  {
-    id: "405406",
-    name: "ANTONIO JR ENARIO ",
-    title: "Supervisor Nursery",
-    group: "NURSERY",
-    file: "",
-    description: "Joining  for ANTONIO JR ENARIO.",
-    status: "N/A",
-  },
-  {
-    id: "405406",
-    name: "ANTONIO JR ENARIO ",
-    title: "Supervisor Nursery",
-    group: "NURSERY",
-    file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
-    description: "Job Offfer for ANTONIO JR ENARIO.",
-    status: "N/A",
-  },
-  {
-    id: "405406",
-    name: "ANTONIO JR ENARIO ",
-    title: "Supervisor Nursery",
-    group: "NURSERY",
-    file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
-    description: "CV for ANTONIO JR ENARIO.",
-    status: "N/A",
-  },
-];
+  /* ... your existing data ... */
+    {
+      id: "407703",
+      name: "Abdul Hakim Omar Al-Kalde",
+      title: "Project Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "ID  For Abdul Hakim Omar Al-Kalde ",
+      status: "N/A",
+    },
+    {
+      id: "407703",
+      name: "Abdul Hakim Omar Al-Kalde",
+      title: "Project Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "Job Offer  For Abdul Hakim Omar Al-Kalde ",
+      status: "N/A",
+    },
+    {
+      id: "407703",
+      name: "Abdul Hakim Omar Al-Kalde",
+      title: "Project Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "Joining from For Abdul Hakim Omar Al-Kalde ",
+      status: "N/A",
+    },
+    {
+      id: "407703",
+      name: "Abdul Hakim Omar Al-Kalde",
+      title: "Project Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "Cv For Abdul Hakim Omar Al-Kalde ",
+      status: "N/A",
+    },
+  
+    {
+      id: "401798",
+      name: "Shoeib Abou Zeid Mahmoud Awad",
+      title: "Operation Manager",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/10hpXcpAfxvaA38QVkoUR7hKeJRHTc_UB/view?usp=sharing",
+      description: "Passprot for Engr. Shoeib Abou Zeid Mahmoud Awad",
+      status: "A/V",
+    },
+    {
+      id: "401798",
+      name: "Shoeib Abou Zeid Mahmoud Awad",
+      title: "Operation Manager",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/1CjrJemtr1DNJZxL5A2ytuy5RitUaR5ob/view?usp=drive_link",
+      description: "Iqama for Engr. Shoeib Abou Zeid Mahmoud Awad",
+      status: "A/V",
+    },
+    {
+      id: "401798",
+      name: "Shoeib Abou Zeid Mahmoud Awad",
+      title: "Operation Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "Joining from for Engr. Shoeib Abou Zeid Mahmoud Awad",
+      status: "N/A",
+    },
+    {
+      id: "401798",
+      name: "Shoeib Abou Zeid Mahmoud Awad",
+      title: "Operation Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "Job for Engr. Shoeib Abou Zeid Mahmoud Awad",
+      status: "N/A",
+    },
+    {
+      id: "401798",
+      name: "Shoeib Abou Zeid Mahmoud Awad",
+      title: "Operation Manager",
+      group: "MANAGMENT",
+      file: "",
+      description: "CV for Engr. Shoeib Abou Zeid Mahmoud Awad",
+      status: "N/A",
+    },
+  
+    {
+      id: "405721",
+      name: "Ashraf Ahmed Abdulhadi",
+      title: "Project Coordinator",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/1MT75u3FnDT4W4GDNz2LEbsb0sM1d8CZX/view?usp=drive_link",
+      description: "Iqama for Engr. Ashraf Ahmed Abdulhadi",
+      status: "A/V",
+    },
+    {
+      id: "405721",
+      name: "Ashraf Ahmed Abdulhadi",
+      title: "Project Coordinator",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
+      description: "Passprot for Engr. Ashraf Ahmed Abdulhadi",
+      status: "A/V",
+    },
+    {
+      id: "405721",
+      name: "Ashraf Ahmed Abdulhadi",
+      title: "Project Coordinator",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
+      description: "Job Offer for Engr. Ashraf Ahmed Abdulhadi",
+      status: "N/A",
+    },
+    {
+      id: "405721",
+      name: "Ashraf Ahmed Abdulhadi",
+      title: "Project Coordinator",
+      group: "MANAGMENT",
+      file: "https://drive.google.com/file/d/1dVVw7_8laQJCUS-cdUfjOJ5pLVIfLc2_/view?usp=drive_link",
+      description: "CV for Engr. Ashraf Ahmed Abdulhadi",
+      status: "N/A",
+    },
+    {
+      id: "403698",
+      name: "Ahmed Al-Dossary",
+      title: "Project Administrator",
+      group: "MANAGMENT",
+      file: "",
+      description: "CV for Ahmed Al-Dossary",
+      status: "N/A",
+    },
+    {
+      id: "403698",
+      name: "Ahmed Al-Dossary",
+      title: "Project Administrator",
+      group: "MANAGMENT",
+      file: "",
+      description: "JoB Offcer for Ahmed Al-Dossary",
+      status: "N/A",
+    },
+    {
+      id: "403698",
+      name: "Ahmed Al-Dossary",
+      title: "Project Administrator",
+      group: "MANAGMENT",
+      file: "",
+      description: "JoB Offcer for Ahmed Al-Dossary",
+      status: "N/A",
+    },
+  
+    {
+      id: "407703",
+      name: "Amir Hossain",
+      title: "Foreman",
+      group: "Airport N & S",
+      file: "https://drive.google.com/file/d/1yvCeguxZt0vdrpRpQa1mtkx2DCrpqF8T/view?usp=drive_link",
+      description: "this one  only Iqama.",
+      status: "A/V",
+    },
+    {
+      id: "405406",
+      name: "ANTONIO JR ENARIO ",
+      title: "Supervisor Nursery",
+      group: "NURSERY",
+      file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
+      description: "Iqama for ANTONIO JR ENARIO.",
+      status: "A/V",
+    },
+    {
+      id: "405406",
+      name: "ANTONIO JR ENARIO ",
+      title: "Supervisor Nursery",
+      group: "NURSERY",
+      file: "",
+      description: "Joining  for ANTONIO JR ENARIO.",
+      status: "N/A",
+    },
+    {
+      id: "405406",
+      name: "ANTONIO JR ENARIO ",
+      title: "Supervisor Nursery",
+      group: "NURSERY",
+      file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
+      description: "Job Offfer for ANTONIO JR ENARIO.",
+      status: "N/A",
+    },
+    {
+      id: "405406",
+      name: "ANTONIO JR ENARIO ",
+      title: "Supervisor Nursery",
+      group: "NURSERY",
+      file: "https://drive.google.com/file/d/1-nNd5dVsq3wWt9xnsr473NSzeWDcpPfd/view?usp=sharing",
+      description: "CV for ANTONIO JR ENARIO.",
+      status: "N/A",
+    },
+  ];
+  
 
 const Files = () => {
   const [search, setSearch] = useState("");
@@ -209,11 +211,13 @@ const Files = () => {
   const filteredData = pdfData.filter((item) => {
     const matchesSearch =
       item.name.toLowerCase().includes(search.toLowerCase()) ||
-      item.id.includes(search);
-      item.description.includes(search);
-      item.title.includes(search);
-      item.group.includes(search);
+      item.id.includes(search) ||
+      item.description.toLowerCase().includes(search.toLowerCase()) ||
+      item.title.toLowerCase().includes(search.toLowerCase()) ||
+      item.group.toLowerCase().includes(search.toLowerCase());
+
     const matchesGroup = groupFilter === "All" || item.group === groupFilter;
+
     return matchesSearch && matchesGroup;
   });
 
@@ -231,11 +235,11 @@ const Files = () => {
       {/* Filter Controls */}
       <div className="mb-4 flex flex-col sm:flex-row gap-3 sm:items-center sm:justify-between">
         {/* Search */}
-        <div className="flex items-center  max-w-md shadow px-3 py-2 rounded-md">
-          <FaSearch className=" mr-2 " />
+        <div className="flex items-center max-w-md shadow px-3 py-2 rounded-md">
+          <FaSearch className="mr-2" />
           <input
             type="text"
-            placeholder="Search by Name or ID"
+            placeholder="Search by Name, ID, Group, Title, Description"
             className="w-full text-sm bg-[#84B941] text-black"
             value={search}
             onChange={(e) => {
@@ -264,7 +268,7 @@ const Files = () => {
 
       {/* Table */}
       <div className="overflow-x-auto">
-        <table className="min-w-full table-auto border text-sm 0">
+        <table className="min-w-full table-auto border text-sm">
           <thead className="bg-gray-200 text-gray-700">
             <tr>
               <th className="p-2 border">#</th>
@@ -279,7 +283,10 @@ const Files = () => {
           </thead>
           <tbody>
             {currentData.map((file, index) => (
-              <tr key={file.id} className="hover:bg-gray-50 text-gray-600">
+              <tr
+                key={`${file.id}-${index}`}
+                className="hover:bg-gray-50 text-gray-600"
+              >
                 <td className="p-2 border text-center">
                   {(currentPage - 1) * itemsPerPage + index + 1}
                 </td>
@@ -288,22 +295,26 @@ const Files = () => {
                 <td className="p-2 border">{file.group}</td>
                 <td className="p-2 border">{file.title}</td>
                 <td className="p-2 border">{file.description}</td>
-                <td className="p-2 border text-center flex justify-around ">
-                  <a
-                    href={file.file}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="px-2 py-1  hover:bg-green-500 text-black rounded border  text-sm"
-                  >
-                    <MdOutlineRemoveRedEye />
-                  </a>
+                <td className="p-2 border text-center flex justify-around">
+                  {file.file ? (
+                    <a
+                      href={file.file}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="px-2 py-1 hover:bg-green-500 text-black rounded border text-sm"
+                    >
+                      <MdOutlineRemoveRedEye />
+                    </a>
+                  ) : (
+                    <span className="text-gray-400 text-xs italic">N/A</span>
+                  )}
                 </td>
                 <td
                   className={`p-2 border text-center text-sm ${
                     file.status === "N/A"
-                      ? "bg-red-500 text-white text-sm"
+                      ? "bg-red-500 text-white"
                       : file.status === "A/V"
-                      ? "bg-emerald-500 text-white" 
+                      ? "bg-emerald-500 text-white"
                       : ""
                   }`}
                 >
@@ -313,7 +324,7 @@ const Files = () => {
             ))}
             {currentData.length === 0 && (
               <tr>
-                <td colSpan="7" className="text-center p-4 text-gray-400">
+                <td colSpan="8" className="text-center p-4 text-gray-400">
                   No data found.
                 </td>
               </tr>
